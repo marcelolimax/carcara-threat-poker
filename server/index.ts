ac98a59552263eb60d32b3297954d4755f49ef78
Marcelo Wanderley Lima <marcelo.suporte.redes@gmail.com>
2025-09-13 20:10:07 -0300

    ajuste de porta

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { generateThreatOptions, analyzeThreats } from './services';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
